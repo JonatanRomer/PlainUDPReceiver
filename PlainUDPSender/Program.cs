c8154ecd354730a641c7545d4f7adeafcf46393d
Jonatan <Jonatan@DESKTOP-3SG699B>
2017-10-10 13:42:12 +0200

    Har laver til A, 6.

--- PlainUDPSender/Program.cs
+++ PlainUDPSender/Program.cs
@@ -4,14 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace PlainUDPReceiver
+namespace PlainUDPSender
 {
     class Program
     {
         private const int PORT = 11001;
         static void Main(string[] args)
         {
-            UDPReceiver service = new UDPReceiver(PORT);
+            UDPSender service = new UDPSender(PORT);
             service.Start();
 
             Console.ReadLine();
---
+++ PlainUDPSender/UDPSender.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlainUDPSender
+{
+    class UDPSender
+    {
+        private readonly int PORT;
+
+        public UDPSender(int port)
+        {
+            PORT = port;
+        }
+
+        public void Start()
+        {
+            //Sender
+            string SenderStr = "Jonatan";
+            byte[] buffer = Encoding.ASCII.GetBytes(SenderStr);
+
+            UdpClient Udp = new UdpClient();
+
+            IPEndPoint OtherEP = new IPEndPoint(IPAddress.Broadcast, PORT);
+            Udp.Send(buffer, buffer.Length, OtherEP);
+
+            IPEndPoint ReceiverEP = new IPEndPoint(IPAddress.Any, 0);
+            byte[] receivebuffer = Udp.Receive(ref ReceiverEP);
+            Console.WriteLine($"UDP datagram received lgt={receivebuffer.Length}");
+            String incommingStr = Encoding.ASCII.GetString(receivebuffer);
+            Console.WriteLine(incommingStr);
+        }
+    }
+}
